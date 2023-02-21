c0caf1300c5243f5d86e957982dba34fd467b07c
4waj <4waj@fresnou.org>
2023-02-21 14:16:40 -0800

    Script3

---
+++ Script3.js
@@ -0,0 +1,80 @@
+//1
+let x = 0;
+while (x < 26){
+    console.log(x);
+    x++;
+}
+
+//2
+let x = 0;
+while (x < 51){
+    console.log(x);
+    x++;
+}
+
+//3
+let x = -10;
+while (x < 11) {
+    console.log(x);
+    x++;
+}
+
+//4
+let x = 0;
+while (x >= -50) {
+    console.log(x);
+    x--;
+}
+
+//5
+let x = 0;
+while (x <= 100) {
+    console.log(x);
+    x+=2;
+}
+
+//6
+let x = 99;
+while (x >= 0) {
+    console.log(x);
+    x-=2;
+}
+
+//7
+let x = 0;
+while (x <= 100) {
+    console.log(x);
+    x+=3;
+}
+
+//8
+let x = 0;
+while (x <= 1000) {
+    console.log(x);
+    x+=5;
+}
+
+//9
+let x = 0;
+while (x <= 100){
+    if (x <= 9) {
+        console.log(x);
+    }
+    else if ( x >= 26){
+        console.log(x);
+    }
+    x++
+}
+
+//10
+let x = 0;
+while (x <= 100){
+    if (x <= 9) {
+        console.log(x);
+    } else if ( x > 20 && x < 40){
+        console.log(x);
+    } else if (x >=51){
+        console.log(x)
+    }
+    x++
+}
---
+++ whileloops1.js
@@ -0,0 +1,12 @@
+const readLine = require('readline-sync');
+
+let x = 0;
+while (x <= 100){
+    if (x <= 9) {
+        console.log(x);
+    }
+    else if ( x >= 26){
+        console.log(x);
+    }
+    x++
+}
