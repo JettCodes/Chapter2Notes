51c1a64d838716d0e951f03c955dedd549ee63d9
4waj <4waj@fresnou.org>
2023-02-23 14:18:31 -0800

    Script4

---
+++ Script4.js
@@ -0,0 +1,14 @@
+//1
+for (let i=0; i <= 150; i++ ){
+    console.log(i);
+}
+
+//2
+for (let i=0; i <= 40; i++ ){
+    console.log(i**2);
+}
+
+//3
+for (let i=0; i <= 30; i++ ){
+    console.log(i**3);
+}
---
+++ forloops1.js
@@ -0,0 +1,5 @@
+for (let i=0; i <= 700; i++ ){
+    if (%5==0){
+        console.log(i);
+    }
+}
